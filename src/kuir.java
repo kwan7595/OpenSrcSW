a8f988343128b691e671b5611db5c6cbd6e71101
KwanHee Lee <kwan7595@naver.com>
2022-03-31 18:11:17 +0900

    3.31(Thu) fixed kuir and searcher

--- src/kuir.java
+++ src/kuir.java
@@ -1,23 +1,37 @@
+import org.xml.sax.SAXException;
+
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 import java.io.IOException;
+import java.util.Objects;
 
 public class kuir {
-    public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, TransformerException, ClassNotFoundException, SAXException {
         String command = args[0];
         String path = args[1];
-
-        if(command.equals("-c")){
-            makeCollection collection = new makeCollection();
-            collection.makeCollection(path);
-        }
-        else if(command.equals("-k")){
-            makeKeyword keyword = new makeKeyword();
-            keyword.makeKeyword(path);
-        }
-        else if(command.equals("-i")){
-            indexer index = new indexer();
-            index.indexer(path);
+        switch (command) {
+            case "-c" -> {
+                makeCollection collection = new makeCollection();
+                collection.makeCollection(path);
+            }
+            case "-k" -> {
+                makeKeyword keyword = new makeKeyword();
+                keyword.makeKeyword(path);
+            }
+            case "-i" -> {
+                indexer index = new indexer();
+                index.indexer(path);
+            }
+            case "-s" -> {
+                if(Objects.equals(args[2], "-q")) {
+                    String q = args[3];
+                    searcher s = new searcher();
+                    s.searcher(path, q);
+                }
+                else{
+                    System.out.println("질의어를 입력해주세요");
+                }
+            }
         }
     }
 }
